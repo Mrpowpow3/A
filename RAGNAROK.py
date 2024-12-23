4d57c7bf8c3e8a5de7e4b6c8871f15d81e374f1b
Mrpowpow3 <dreamxvps03@outlook.com>
2024-12-23 21:26:05 +0530

    Update RAGNAROK.py

--- RAGNAROK.py
+++ RAGNAROK.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot tokSasen here
-bot = telebot.TeleBot('7238502739:AAHy5YDUshfC-Ra-v5Cswjoi3wcfB_OjLTg')
+bot = telebot.TeleBot('7825528822:AAHofr8lIk3XgjDW0f0Gsdq_tm0Y4j9b0zg')
 
 # Admin user IDs
-admin_id = ["5599402910"]
+admin_id = ["6636078430"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
